be61bb14d0f1b58aee756bd102162e115aa74a30
Ankit <2015.ankit@gmail.com>
2016-04-13 17:32:28 -0700

    Update conf.js

--- www/test/functional/conf.js
+++ www/test/functional/conf.js
@@ -1,11 +1,16 @@
 exports.config = {
-  directConnect : true,
-  framework: 'jasmine2',
-  specs: ['report.spec.js'],
-  baseUrl: 'http://localhost:8100/vamosjuntas'
-};
-if (process.env.SNAP_CI) {
-    exports.config.directConnect = true;
-    exports.config.chromeDriver = '/usr/local/bin/chromedriver';
-}
+  capabilities: {
+     'browserName': 'chrome'
+   },
+    framework: 'jasmine',
+   seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['report.spec.js'],
+  };
+ 
+ if (process.env.SNAP_CI) {
+   exports.config.directConnect = true;
+   exports.config.chromeDriver = '/usr/local/bin/chromedriver';
+ } else {
+   exports.config.seleniumAddress = 'http://localhost:4444/wd/hub';
+ }
 seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.52.0.jar'
